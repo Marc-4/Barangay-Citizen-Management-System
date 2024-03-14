5420cd200ac9b552c3256e8ecef7cc300012cfa4
Marc <blaze.verdugo.s@gmail.com>
2024-03-14 20:56:08 +0800

    custom animation

--- Client/src/components/popups/NotificationPopup.jsx
+++ Client/src/components/popups/NotificationPopup.jsx
@@ -1,6 +1,6 @@
 import { Box, Heading, Text } from '@chakra-ui/react'
 import NotificationCard from '../cards/NotificationCard'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { Component, forwardRef } from 'react'
 
 const NotificationPopup = forwardRef(
@@ -9,43 +9,55 @@ const NotificationPopup = forwardRef(
       (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
     )
 
+    const popupVariants = {
+      hidden: { opacity: 0, scale: 0, x: '50%', y: '-50%' },
+      visible: { opacity: 1, scale: 1, x: '0%', y: '0%', transition: { duration: 0.1 } },
+    }
+
     return (
       <>
-        <Box
-          ref={ref}
-          display={isOpen ? 'block' : 'none'}
-          p={'10px'}
-          zIndex={2}
-          bg={'background.50'}
-          h={'600px'}
-          minW={'400px'}
-          borderRadius={'10px'}
-          overflowY={'auto'}
-          overflowX={'clip'}
-          position={'absolute'}
-          top={'60px'}
-          right={rightOffset + 145}
-          css={{
-            '::-webkit-scrollbar': {
-              width: '1px',
-            },
-          }}
-        >
-          <Heading textColor={'text.main'}>Notifications</Heading>
-          {notifications.length !== 0 ? (
-            sortedNotifications.map((notification) => (
-              <NotificationCard
-                key={notification._id}
-                data={notification}
-                onNotificationClick={onNotificationClick}
-              />
-            ))
-          ) : (
-            <Text fontWeight={'semibold'} fontSize={'xl'} mt={'25px'} textAlign={'center'}>
-              No notifications found
-            </Text>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              key='notificationPopup'
+              ref={ref}
+              initial='hidden'
+              animate='visible'
+              exit='hidden'
+              variants={popupVariants}
+              transition={{ duration: 0.2 }}
+              style={{
+                display: 'block',
+                position: 'absolute',
+                top: '60px',
+                right: rightOffset + 145,
+                zIndex: 2,
+                background: '#f2f2f2',
+                padding: '10px',
+                borderRadius: '10px',
+                overflowY: 'auto',
+                overflowX: 'clip',
+                minWidth: '400px',
+                height: '600px',
+              }}
+            >
+              <Heading textColor={'text.main'}>Notifications</Heading>
+              {notifications.length !== 0 ? (
+                sortedNotifications.map((notification) => (
+                  <NotificationCard
+                    key={notification._id}
+                    data={notification}
+                    onNotificationClick={onNotificationClick}
+                  />
+                ))
+              ) : (
+                <Text fontWeight={'semibold'} fontSize={'xl'} mt={'25px'} textAlign={'center'}>
+                  No notifications found
+                </Text>
+              )}
+            </motion.div>
           )}
-        </Box>
+        </AnimatePresence>
       </>
     )
   }
