76fc28605d879c52c5819d72a66ecc60b66f037c
Marc <blaze.verdugo.s@gmail.com>
2024-03-27 00:38:03 +0800

    rotated pdf file disply

--- Client/src/components/modals/ExportModal.jsx
+++ Client/src/components/modals/ExportModal.jsx
@@ -21,25 +21,41 @@ import {
   Tbody,
   Th,
   Td,
+  Select,
+  useToast,
+  Toast,
 } from '@chakra-ui/react'
 
 import { useState, useEffect } from 'react'
 import callAPI from '../../utils/callAPI'
 import Searchbar from '../Searchbar'
 import { pdfjs, Document, Page } from 'react-pdf'
-import { PDFDocument, rgb } from 'pdf-lib'
-const ExportModal = ({ isOpen, onClose, user, onUpdate, role }) => {
+import { PDFDocument, rgb, rotateInPlace } from 'pdf-lib'
+import CustomTable from '../CustomTable'
+import RefreshButton from '../RefreshButton'
+const ExportModal = ({ isOpen, onClose, users, onUpdate, role }) => {
   // pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   //   'pdfjs-dist/build/pdf.worker.min.js',
   //   import.meta.url
   // ).toString()
 
   pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
-  const [pdfUrl, setPdfUrl] = useState('/BarangayForms.pdf')
+
+  const pdfUrls = { ' ': '', single: '/BarangayForms-5.pdf', family: '/BarangayForms-4.pdf' }
+  const [pdfUrl, setPdfUrl] = useState('')
   const [numPages, setNumPages] = useState(null)
+  const [userList, setUserList] = useState(users)
   const [pageNumber, setPageNumber] = useState(1)
-  const [userData, setUserData] = useState(null)
-  const accountRole = localStorage.getItem('userRole')
+  const toast = useToast()
+  const [refreshCounter, setRefreshCounter] = useState(0)
+
+  useEffect(() => {
+    setUserList(users)
+  }, [users])
+
+  useEffect(() => {
+    setUserList(users)
+  }, [refreshCounter])
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages)
@@ -75,7 +91,7 @@ const ExportModal = ({ isOpen, onClose, user, onUpdate, role }) => {
 
   const renderPdfWithUserData = async () => {
     try {
-      const templatePdfBytes = await fetch('/BarangayForms.pdf').then((res) => res.arrayBuffer())
+      const templatePdfBytes = await fetch(pdfUrl).then((res) => res.arrayBuffer())
       const modifiedPdfBytes = await populatePdfWithData(templatePdfBytes, userData)
 
       // Convert modified PDF bytes to a Blob and create a URL for rendering
@@ -88,41 +104,97 @@ const ExportModal = ({ isOpen, onClose, user, onUpdate, role }) => {
     }
   }
 
-  useEffect(() => {
-    if (userData) {
-      renderPdfWithUserData()
+  const filterTable = async (query, values, sex) => {
+    const route = `http://localhost:3000/api/admin/users/search?query=${query}&params=${values}&sex=${sex}`
+    const method = 'GET'
+
+    try {
+      const response = await callAPI(null, method, route)
+
+      if (response.result == 'OK') {
+        console.log(response.payload)
+        setUserList(response.payload)
+      } else {
+        toast({
+          title: 'error',
+          description: 'Connection Error. Please try again.',
+          status: 'error',
+          duration: '5000',
+          isClosable: 'true',
+          position: 'bottom-right',
+        })
+      }
+    } catch (error) {
+      toast({
+        title: 'error',
+        description: error,
+        status: 'error',
+        duration: '5000',
+        isClosable: 'true',
+        position: 'bottom-right',
+      })
     }
-  }, [userData])
+  }
+
+  // useEffect(() => {
+  //   if (userList) {
+  //     renderPdfWithUserData()
+  //   }
+  // }, [userList])
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size={'2xl'} closeOnOverlayClick={false}>
+    <Modal isOpen={isOpen} onClose={onClose} size={'6xl'} closeOnOverlayClick={false}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Export to PDF</ModalHeader>
+        <ModalHeader fontSize={'3xl'} fontWeight={'bold'}>
+          Export to PDF
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+          <Box display={'flex'} gap={'25px'}>
+            <Searchbar forFilter searchHandler={filterTable} />
+            <RefreshButton refreshCounter={refreshCounter} setRefreshCounter={setRefreshCounter} />
+            <Select
+              w={'200px'}
+              placeholder='Select Type of PDF'
+              onChange={(e) => {
+                setPdfUrl(pdfUrls[e.target.value])
+              }}
+            >
+              <option value='single'>Single</option>
+              <option value='family'>Family</option>
+            </Select>
+          </Box>
+
+          <CustomTable forFilter users={userList} />
+          <Document
+            rotate={pdfUrl == '/BarangayForms-4.pdf' ? 90 : 0}
+            file={pdfUrl}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
             <Page pageNumber={pageNumber} />
           </Document>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-          <Box>
-            <Button onClick={goToPreviousPage} disabled={pageNumber === 1}>
-              Previous Page
-            </Button>
-            <Button onClick={goToNextPage} disabled={pageNumber === numPages}>
-              Next Page
-            </Button>
-            <Text>Go to Page:</Text>
-            <input
-              type='number'
-              value={pageNumber}
-              onChange={handlePageInputChange}
-              min={1}
-              max={numPages}
-            />
-          </Box>
+          {pdfUrl != '' && pdfUrl != null && (
+            <Box>
+              <p>
+                Page {pageNumber} of {numPages}
+              </p>
+              <Button onClick={goToPreviousPage} disabled={pageNumber === 1}>
+                Previous Page
+              </Button>
+              <Button onClick={goToNextPage} disabled={pageNumber === numPages}>
+                Next Page
+              </Button>
+              <Text>Go to Page:</Text>
+              <input
+                type='number'
+                value={pageNumber}
+                onChange={handlePageInputChange}
+                min={1}
+                max={numPages}
+              />
+            </Box>
+          )}
         </ModalBody>
       </ModalContent>
     </Modal>
